ca30060bce2324772d8ae3649145f46756ff1cb0
Norman Joyner <norman.joyner@gmail.com>
2014-05-05 13:03:38 -0400

    Fix location redirect

--- expresspial.js
+++ expresspial.js
@@ -4,8 +4,16 @@ module.exports = function(espial){
 
         default: function(req, res, next){
             if(!espial.is_master()){
-                var master = espial.master;
-                res.location(master.ip);
+                var master = espial.get_master();
+
+                var port = req.headers.host.split(":");
+                if(port.length > 1)
+                    port = port[1];
+                else
+                    port = 80;
+
+                var location = [req.protocol, "://", master.ip, ":", port, req.url].join("");
+                res.redirect(307, location);
             }
             else
                 next();
