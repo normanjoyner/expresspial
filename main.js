e3e9ba6104e811b157e825637d459a2ee9cc1666
Norman Joyner <norman.joyner@gmail.com>
2014-05-05 12:09:33 -0400

    Fixes typo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 exports = module.exports = function(espial){
-    var expresspial = require([__dirname, "expresspial"].join("/")(espial);
+    var expresspial = require([__dirname, "expresspial"].join("/"))(espial);
     return expresspial;
 }
