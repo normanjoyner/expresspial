ec9c6388246ca308e2a230d734f3440f4e2de48d
Norman Joyner <norman.joyner@gmail.com>
2014-05-09 15:05:55 -0400

    Add redirect_to_master / redirect_to_slave methods

--- expresspial.js
+++ expresspial.js
@@ -1,12 +1,14 @@
+var _ = require("lodash");
+
 module.exports = function(espial){
 
     return {
 
-        default: function(req, res, next){
+        redirect_to_master: function(req, res, next){
             if(!espial.is_master()){
                 var master = espial.get_master();
 
-                if(master.ip == undefined)
+                if(_.isUndefined(master.ip))
                     res.send(503);
                 else{
                     var port = req.headers.host.split(":");
@@ -16,6 +18,27 @@ module.exports = function(espial){
                         port = 80;
 
                     var location = [req.protocol, "://", master.ip, ":", port, req.url].join("");
+                    res.redirect(307, location);
+                }
+            }
+            else
+                return next();
+        },
+
+        redirect_to_slave: function(req, res, next){
+            if(espial.is_master()){
+                var slave = _.sample(espial.get_nodes());
+
+                if(!_.has(slave, "ip"))
+                    res.send(503);
+                else{
+                    var port = req.headers.host.split(":");
+                    if(port.length > 1)
+                        port = port[1];
+                    else
+                        port = 80;
+
+                    var location = [req.protocol, "://", slave.ip, ":", port, req.url].join("");
                     res.redirect(307, location);
                 }
             }
